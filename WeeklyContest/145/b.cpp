34c287d827885de164c4e85fa33afdad18635119
ahjeffre <ahjeffre@amazon.com>
2019-08-15 13:30:36 -0700

    147: lca, 145: prefix sum matrix

--- WeeklyContest/145/b.cpp
+++ WeeklyContest/145/b.cpp
@@ -1,47 +1,49 @@
 class Solution {
 public:
-    map<TreeNode *, int> depth;
-    map<TreeNode *, TreeNode *> parent;
+    map <TreeNode*, TreeNode*> parent;
+    map <TreeNode*, int> depth;
     int deepest;
 
-    void dfs(TreeNode *node, TreeNode *par) {
-        if (node == nullptr)
-            return;
+    void dfs(TreeNode* root, TreeNode* prev) {
+        if (!root) return;
 
-        parent[node] = par;
-        depth[node] = par == nullptr ? 0 : depth[par] + 1;
-        deepest = max(deepest, depth[node]);
-        dfs(node->left, node);
-        dfs(node->right, node);
+        parent[root] = prev;
+        depth[root] = prev == nullptr ? 0 : depth[prev] + 1;
+
+        if (depth[root] > deepest)
+            deepest = depth[root];
+
+        dfs(root->left, root);
+        dfs(root->right, root);
     }
 
-    TreeNode *lca(TreeNode *a, TreeNode *b) {
+    TreeNode* lca(TreeNode* a, TreeNode* b) {
         while (a != b) {
             if (depth[a] < depth[b])
                 swap(a, b);
 
             a = parent[a];
         }
-
         return a;
     }
 
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        depth.clear();
-        parent.clear();
+
         deepest = 0;
         dfs(root, nullptr);
-        vector<TreeNode *> leaves;
 
-        for (pair<TreeNode *, int> p : depth)
+        vector<TreeNode*> same_level;
+
+        for (auto p : depth) {
             if (p.second == deepest)
-                leaves.push_back(p.first);
+                same_level.push_back(p.first)
+        }
 
-        TreeNode *answer = leaves[0];
+        TreeNode* res = same_level[0];
+        for (int i = 1; i < same_level.size(); i++) {
+            res = lca(res, same_level[i]);
+        }
 
-        for (int i = 1; i < (int) leaves.size(); i++)
-            answer = lca(answer, leaves[i]);
-
-        return answer;
+        return res;
     }
 };
